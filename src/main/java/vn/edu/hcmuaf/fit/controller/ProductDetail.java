bc290776dfd127b5d4483b6ea5851e87f8f9cca2
hothanhnghia-nlu <nghiant369@gmail.com>
2023-06-23 15:17:10 +0700

    Update ProductDetail

--- src/main/java/vn/edu/hcmuaf/fit/controller/ProductDetail.java
+++ src/main/java/vn/edu/hcmuaf/fit/controller/ProductDetail.java
@@ -1,12 +1,15 @@
 package vn.edu.hcmuaf.fit.controller;
 
+import vn.edu.hcmuaf.fit.bean.Comment;
 import vn.edu.hcmuaf.fit.bean.Product;
+import vn.edu.hcmuaf.fit.service.CommentService;
 import vn.edu.hcmuaf.fit.service.ProductService;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "detailControl", value = "/detail")
 public class ProductDetail extends HttpServlet {
@@ -15,8 +18,10 @@ public class ProductDetail extends HttpServlet {
         String id = request.getParameter("pid");
         ProductService pro = new ProductService();
         Product product = pro.getProductByID(id);
+        List<Comment> comment = CommentService.getInstance().listAllComment();
         request.setAttribute("list",ProductService.getInstance().getTopSellerAsVendorID(3,product.getVendor()));
         request.setAttribute("detail",product);
+        request.setAttribute("comment", comment);
         request.getRequestDispatcher("product-detail.jsp").forward(request, response);
     }
 
